d43712d209e92d5cff14b07dddc78fda65b8c941
runav <75965709+runav@users.noreply.github.com>
2021-01-19 22:23:45 -0800

    new app

---
+++ src/components/Movies.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, useLayoutEffect } from "react";
+
+import * as Movie from '../services/fakeMovieService';
+
+const Movies = () => {
+
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = async () => {
+        const response = await Movie.getMovies();
+        setMovies(response);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    const handleDelete = (movie) => {
+        const newMovies = movies.filter(m => m._id !== movie._id);
+        setMovies(newMovies);
+        console.log(movie);
+    }
+
+    return (
+        <>
+
+<div>There are {movies.length} movies in database</div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {movies.map(movie => {
+                        const { _id, title, genre, numberInStock, dailyRentalRate } = movie;
+                        return (
+                            <tr key={_id}>
+                                <td>{title}</td>
+                                <td>{genre.name}</td>
+                                <td>{numberInStock}</td>
+                                <td>{dailyRentalRate}</td>
+                                <td><button className="btn btn-danger btn-sm" onClick={() => handleDelete(movie)}>Delete</button></td>
+                            </tr>
+                        )
+
+                    })}
+                </tbody>
+            </table>
+            <div>
+
+            </div>
+        </>
+    );
+}
+
+
+export default Movies
